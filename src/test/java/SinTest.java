8b94db5b58e5e19c8c0578985a1337dffe708291
Ivan Dehtiarov <romasan221@gmail.com>
2021-12-03 01:09:53 +0200

    Added SinTest

--- src/test/java/SinTest.java
+++ src/test/java/SinTest.java
@@ -1,9 +1,70 @@
-import org.junit.Test;
+import com.company.*;
+import org.junit.*;
+
+import java.text.NumberFormat;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SinTest {
-    @Test
-    public void sas()
-    {
+    private Sin sin;
+    private Object result;
 
+    @BeforeClass
+    public static void GlobalSetUp() {
+        System.out.println("Starting Sin Tests...");
+    }
+
+    @AfterClass
+    public static void GlobalFinish() {
+        System.out.println("Sin Tests have finished!");
+
+    }
+
+    @Before
+    public void SetUp() {
+        sin = new Sin(new Const(90),1);
+        result = new Object();
+    }
+
+    @After
+    public void Finish() {
+        System.out.println("Test method has finished.");
+    }
+
+    @Test
+    public void CalculateMethodReturnDoubleType() {
+        result = sin.calculate(1);
+        assertTrue("method must return double", result instanceof Double);
+    }
+
+    @Test
+    public void CalculateMethodTest() {
+        result = Math.sin(90);
+        assertEquals("sin(90) = 1", result, sin.calculate(1));
+    }
+
+    @Test
+    public void DerivativeMethodReturnMultiplicationType() {
+        result = sin.derivative();
+        assertTrue("method must return Multiplication", result instanceof Multiplication);
+    }
+
+    @Test
+    public void DerivativeMethodTest() {
+        result = -0.0;
+        assertEquals("(sin(90))` = 0", result, sin.derivative().calculate(1));
+    }
+
+    @Test
+    public void ToPrettyStringReturnStringType() {
+        result = sin.toPrettyString(NumberFormat.getInstance());
+        assertTrue("method must return String", result instanceof String);
+    }
+
+    @Test
+    public void ToPrettyStringTest() {
+        result = "sin(90)";
+        assertEquals("must be sin(90)", result, sin.toPrettyString(NumberFormat.getInstance()));
     }
 }
